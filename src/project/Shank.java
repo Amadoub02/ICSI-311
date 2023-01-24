d75fd1848f84369417d50f3d167186fba4730ad2
Amadou Balde <abalde.ny@gmail.com>
2023-01-24 16:44:01 -0500

    first commit

---
+++ src/project/Shank.java
@@ -0,0 +1,32 @@
+package project;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Shank {
+    public static void main(String[] args) {
+        Path path = Paths.get("shank.txt");
+        //List<String> shankLines = Files.readAllLines(path, StandardCharsets.UTF_8);
+       // List<Token> tokenList = new ArrayList<>();
+
+
+        if (args.length == 0) {
+            System.out.println("No argument was detected ");
+        }
+        if (args.length < 1) {
+            System.out.println("More than one argument was detected ");
+        } else {
+            try {
+                List<String> fileLines = Files.readAllLines(path, StandardCharsets.UTF_8);
+                List<Token> tokenList = new ArrayList<>();
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
